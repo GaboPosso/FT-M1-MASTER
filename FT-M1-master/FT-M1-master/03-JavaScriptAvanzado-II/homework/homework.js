028497c64af2041c3952e4cfc96a667a66ff988a
GaboPosso <gabopos@gmail.com>
2022-06-09 10:58:26 -0500

    Upload tested JsAvanzado-II  Homework.js

--- FT-M1-master/FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
+++ FT-M1-master/FT-M1-master/03-JavaScriptAvanzado-II/homework/homework.js
@@ -19,23 +19,23 @@ function counter() {
   otroContador()      // 2
   otroContador()      // 3
    */
-  function counter() { 
+  
     //Closures: Three conditions are required:  1. A function wrote inside another function(nested)
     /*------------------------------------------2. A variable declared in the parent function, which is called 
       ---------------------------------------------inside the nested function. 	
       ------------------------------------------3. Invoke the nested function from outer scope.*/								
-        let contador = 0; // Variable declared in the parent function Scope, which is referred in the nested 														//function
-        return function incrementa(){  //Nested Function
-          contador = contador + 1;	//Call variable declared in parent function 
-          return  contador; 
-      }
+  let contador = 0; // Variable declared in the parent function Scope, which is referred in the nested 														//function
+  return function (){  //Nested Function
+    contador++;	//Call variable declared in parent function 
+    return  contador; 
     }
-    const nuevoContador = counter(); //Despite the variable 'contador' is inside the parent function scope, an 
+  }
+  const nuevoContador = counter(); //Despite the variable 'contador' is inside the parent function scope, an 
     //it's not reachable from global scope, we can define a new variable with global scope, as we save the 
     //function in the global variable, we can call the nested function.
-    const otroContador = counter(); //A new variable with the saved function creates a new Execution Context.
+  const otroContador = counter(); //A new variable with the saved function creates a new Execution Context.
 
-}
+
 
 function cacheFunction(cb) {
   /*
@@ -51,15 +51,21 @@ function cacheFunction(cb) {
     return n * n
   }
 
-  const squareCache = cacheFunction(square)
+  const squareCache = cacheFuncnotion(square)
 
   squareCache(5)    // invocará a square(5), almacenará el resultado y lo retornará
   squareCache(5)    // no volverá a invocar a square, simplemente buscará en la caché cuál es el resultado de square(5) y lo retornará (tip: si usaste un objeto, podés usar hasOwnProperty) 
 
   */
-  
-
-  
+  var cache = {}; //Create an empty object
+  return function(arg) { //return  function  with property arg
+    if (cache.hasOwnProperty(arg)){ // if cache has arg as property inside
+      return cache[arg]; // then it returns the arg
+    } else {
+      cache[arg] = cb(arg); // if it hasn't arg as argument
+      return cache[arg];
+    }
+  }; 
 }
 
 // Bind
@@ -78,6 +84,8 @@ function getNombre() {
   return this.nombre;
 }
 
+
+
 /*
   Ejercicio 3
 
@@ -86,23 +94,26 @@ function getNombre() {
   Usando el método bind() guardar, en las dos variables declaradas a continuación, dos funciones que actúen como getNombre pero retornen el nombre del instructor y del alumno, respectivamente.
 */
 
-let getNombreInstructor;
-let getNombreAlumno;
+let getNombreInstructor = getNombre.bind(instructor);
+let getNombreAlumno = getNombre.bind(alumno);
 
 /*
   Ejercicio 4
   
-  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos, respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los otros argumentos habrán sido "bindeados". 
+  Sin modificar la función crearCadena, usar bind para guardar, en las tres variables declaradas a continuación, 
+  tres funciones que retornen una cadena (string) y el delimitador especificado (asteriscos, guiones, y guiones bajos,
+    respectivamente). Las funciones obtenidas deberían recibir solamente un argumento - la cadena de texto - ya que los
+    otros argumentos habrán sido "bindeados". 
 */
 
 function crearCadena(delimitadorIzquierda, delimitadorDerecha, cadena) {
   return delimitadorIzquierda + cadena + delimitadorDerecha;
 }
 
-let textoAsteriscos;
-let textoGuiones;
-let textoUnderscore;
-
+let textoAsteriscos = crearCadena.bind(null, "*", "*");
+let textoGuiones = crearCadena.bind(this, '-', '-');
+let textoUnderscore = crearCadena.bind(this, '_', '_');
+ 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
